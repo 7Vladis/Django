df30eb2edce3ef03d2f53941b31b1206ac5636a7
Vlad <vlad_sudnitsyn@mail.ru>
2025-03-08 20:49:18 +0500

    Updated project files

--- echoserver/books/views.py
+++ echoserver/books/views.py
@@ -1,4 +1,8 @@
+from pydoc import pager
+
+from django.contrib.admin.templatetags.admin_list import pagination
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import  Paginator, EmptyPage, PageNotAnInteger
 from .models import Books
 from .forms import BookForm
 
@@ -22,6 +26,24 @@ def delete_book(request, pk):
         return redirect('book_list')
     return redirect('book_list')
 
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/add_book.html', {'form':form})
+
 def book_list(request):
     books = Books.objects.all()
-    return render(request, 'books/books_list.html', {'books': books})
+    paginator = Paginator(books, 5)
+    page = request.GET.get('page')
+    try:
+        books_page = paginator.get_page(page)
+    except PageNotAnInteger:
+        books_page = paginator.page(1)
+    except EmptyPage:
+        books_page = paginator.page(paginator.num_pages)
+    return render(request, 'books/books_list.html', {'books': books_page})
